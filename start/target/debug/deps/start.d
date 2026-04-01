e6c5d6c5645949d2890bc8a74765fbdebf0176fe
kumar3000 <sidarthoscar15@gmail.com>
2026-04-01 10:40:43 -0400

    Changed .gitignore

---
+++ start/target/debug/deps/start.d
@@ -0,0 +1,5 @@
+C:\Users\sidar\Documents\Code\GITHUB\learning_rust\start\target\debug\deps\start.d: src\main.rs
+
+C:\Users\sidar\Documents\Code\GITHUB\learning_rust\start\target\debug\deps\start.exe: src\main.rs
+
+src\main.rs:
---
+++ start/target/debug/start.d
@@ -0,0 +1,1 @@
+C:\Users\sidar\Documents\Code\GITHUB\learning_rust\start\target\debug\start.exe: C:\Users\sidar\Documents\Code\GITHUB\learning_rust\start\src\main.rs
